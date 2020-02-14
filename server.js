303f6b8a281250879c430a7cec206d93bdd862e6
Ashwin Gupta <ashwingupta2000@gmail.com>
2020-02-13 17:34:44 -0800

    init

---
+++ server.js
@@ -0,0 +1,60 @@
+
+var admins = ["ashwingupta2000"];
+
+
+var express = require("express");
+var bodyParser = require("body-parser");
+var glob = require("glob");
+var fs = require('fs');
+var app = express();
+var pwd = "never-use-me";
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/handle',function(request,response){
+  //console.log(request);
+  
+ // console.log("recieved POST");
+   //response.sendStatus(200);
+  
+  
+  
+  if(request.body.command == "/setpwd") {
+    if(admins.includes(request.body.user_name)) {
+      pwd = request.body.text; 
+      response.send("Set password to: " + pwd);
+      console.log("Set password to: " + pwd);
+    } else {
+      response.send("Failed to set password, you don't have permission");
+      console.log(request.body.user_name + " does not have permission to set pwd");
+    }
+  }
+  
+  if(request.body.command == "/login") {
+    if(request.body.text == pwd) {
+      var today = new Date();
+      //console.log(today);
+      var date = "" + today.getFullYear() + "-" +(today.getMonth()+1) + "-" + today.getDate();
+      glob("logs/*.json", function (er, files) {
+		console.log(files);	
+	  });
+      
+    } else {
+      response.send("Failed to login, bad password");
+      console.log(request.body.user_name + "Failed to login, bad password");
+    }
+     
+  }  
+  
+  
+ 
+  response.end();
+  
+});
+
+app.listen(8000,function(){
+
+  
+  console.log("Started Server on PORT 8000");
+}) 
