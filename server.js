3389b83d4db5add702251d4c222a70bf1478fe9a
eho99 <eho88@outlook.com>
2020-05-06 17:33:02 -0700

    test

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-
 var admins = ["ashwingupta2000"];
 
+// 5:32 5/6/20 Eric
 
 var express = require("express");
 var bodyParser = require("body-parser");
@@ -12,67 +12,66 @@ var pwd = "never-use-me";
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.post('/handle',function(request,response){
-  //console.log(request);
-  
- // console.log("recieved POST");
-   //response.sendStatus(200);
-  
-  
-  
-  if(request.body.command == "/setpwd") {
-    if(admins.includes(request.body.user_name)) {
-      pwd = request.body.text; 
-      response.send("Set password to: " + pwd);
-      console.log("Set password to: " + pwd);
-    } else {
-      response.send("Failed to set password, you don't have permission");
-      console.log(request.body.user_name + " does not have permission to set pwd");
-    }
-  }
-  
-  if(request.body.command == "/login") {
-    if(request.body.text == pwd) {
-      var today = new Date();
-      //console.log(today);
-      var date = "" + today.getFullYear() + "-" +(today.getMonth()+1) + "-" + today.getDate();
-	 
-      var file = glob.sync("logs/*.json");
-		
-	  
-	  console.log(file);	
+app.post('/handle', function(request, response) {
+    //console.log(request);
 
-      if(file.includes("logs/"+date+".json")) {
-		  var data = fs.readFileSync("logs/"+date+".json");
-	      var inputD = JSON.parse(data);
-	  }
-	  else {
-		  var inputD = {
-			 "students": []
-		  };
-	  }
-	  console.log(inputD); 
-	  inputD["students"].push(request.body.user_name);
-	  console.log(inputD); 
-	  var outData = JSON.stringify(inputD);
-	  fs.writeFileSync("logs/"+date+".json", outData);
-	  response.send("Login Successful");
+    // console.log("recieved POST");
+    //response.sendStatus(200);
 
-    } else {
-      response.send("Failed to login, bad password");
-      console.log(request.body.user_name + "Failed to login, bad password");
+
+
+    if (request.body.command == "/setpwd") {
+        if (admins.includes(request.body.user_name)) {
+            pwd = request.body.text;
+            response.send("Set password to: " + pwd);
+            console.log("Set password to: " + pwd);
+        } else {
+            response.send("Failed to set password, you don't have permission");
+            console.log(request.body.user_name + " does not have permission to set pwd");
+        }
     }
-     
-  }  
-  
-  
- 
-  response.end();
-  
+
+    if (request.body.command == "/login") {
+        if (request.body.text == pwd) {
+            var today = new Date();
+            //console.log(today);
+            var date = "" + today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+
+            var file = glob.sync("logs/*.json");
+
+
+            console.log(file);
+
+            if (file.includes("logs/" + date + ".json")) {
+                var data = fs.readFileSync("logs/" + date + ".json");
+                var inputD = JSON.parse(data);
+            } else {
+                var inputD = {
+                    "students": []
+                };
+            }
+            console.log(inputD);
+            inputD["students"].push(request.body.user_name);
+            console.log(inputD);
+            var outData = JSON.stringify(inputD);
+            fs.writeFileSync("logs/" + date + ".json", outData);
+            response.send("Login Successful");
+
+        } else {
+            response.send("Failed to login, bad password");
+            console.log(request.body.user_name + "Failed to login, bad password");
+        }
+
+    }
+
+
+
+    response.end();
+
 });
 
-app.listen(8000,function(){
+app.listen(8000, function() {
 
-  
-  console.log("Started Server on PORT 8000");
-}) 
+
+    console.log("Started Server on PORT 8000");
+})
