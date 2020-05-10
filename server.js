19f66084d35902cab4c16ebfe2703ec3991b4078
ishanpranav <64936129+ishanpranav@users.noreply.github.com>
2020-05-10 11:17:21 -0700

    Update server.js
    UI Interoperability

--- server.js
+++ server.js
@@ -147,7 +147,23 @@ app.post('/handle', function(request, response) {
 
     }
 
+    /// Works with UI - can someone verify that this will return the json contents of the requested file?
+    // Ensure that the UI application is the source of the request.
+    if (request.body.token == "UI") {
 
+        // Exclude '/' to disallow users from sending these commands.
+        if (request.body.command == "attendance" || request.body.command == "poll") {
+            var file = glob.sync(request.body.command + "_logs/*.json");
+
+            // e.g. attendance_logs/2020-10-31.json
+            var fileName = request.body.command + "_logs/" + request.body.text + ".json";
+
+            if (file.includes(fileName)) {
+                response.send(fs.readFileSync(fileName));
+            }
+        }
+    }
+    /// Works with UI
 
     response.end();
 
