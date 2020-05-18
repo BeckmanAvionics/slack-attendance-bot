03a7e4d6853896c98c27d986c22938f40adac5dd
jonathon-b <46765111+jonathon-b@users.noreply.github.com>
2020-05-18 15:15:57 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,3 @@
-#change cd to the directory in which server.js is located
 #for ease fo use, configure this to run at startup (https://linuxtechlab.com/executing-commands-scripts-at-reboot/)
 
-cd slack-bot/slack-bot
 node server.js
